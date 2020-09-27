abd0197741a7ac86f55afc55ade26bebe503eaa3
Entalizen <danielarya185@gmail.com>
2020-09-27 23:41:10 +0700

    Add(handler) controller for inserting user

---
+++ cmd/application/handler/insert_user.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"io/ioutil"
+
+	"github.com/Satssuki/CleanArchSimplified/internal/domain"
+	"github.com/Satssuki/CleanArchSimplified/internal/interfaces"
+	"github.com/labstack/echo/v4"
+)
+
+// InsertUser route for inserting new user
+func InsertUser(c echo.Context) error {
+	logic := domain.CreateUserLogic()
+	bytes, err := ioutil.ReadAll(c.Request().Body)
+	var statusCode int = 400
+	if err == nil {
+		err = interfaces.ComposeToConcreteObject(logic, &bytes)
+		if err == nil {
+			var status string
+			status, err = logic.RegisterIfNotExist()
+
+			if err == nil {
+				statusCode, message := ParseDomainMessage(status)
+				return c.String(statusCode, message)
+			}
+		}
+	}
+	return c.NoContent(statusCode)
+}

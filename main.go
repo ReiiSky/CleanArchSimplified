8982f568256ea469cf890e89f1d4cc47dcc14fae
Entalizen <danielarya185@gmail.com>
2020-09-27 23:49:12 +0700

    applying server in main file

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/Satssuki/CleanArchSimplified/cmd/application"
+	config "github.com/Satssuki/CleanArchSimplified/pkg/system"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(config.CrossPlatformPath("config/dev.env"))
+	application.RunServer(os.Getenv("PORT"))
 }

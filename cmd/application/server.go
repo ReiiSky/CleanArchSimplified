a6b013da97e04e67b7c4f321b604205131ca4472
Entalizen <danielarya185@gmail.com>
2020-09-27 23:41:53 +0700

    Add(app) function to run server and register user controller

---
+++ cmd/application/server.go
@@ -0,0 +1,24 @@
+package application
+
+import (
+	"os"
+
+	"github.com/Satssuki/CleanArchSimplified/cmd/application/handler"
+	"github.com/Satssuki/CleanArchSimplified/internal/infrastructure"
+	"github.com/labstack/echo/v4"
+)
+
+// RunServer run server with specified port
+func RunServer(port string) {
+	infrastructure.SetupMongoConnection(os.Getenv("DBNAME"), os.Getenv("MONGOURL"))
+
+	server := echo.New()
+	defer server.Start(port)
+
+	registerRoute(server)
+
+}
+
+func registerRoute(root *echo.Echo) {
+	root.POST("/user", handler.InsertUser)
+}

123c06d2daba687e1c0dc00f7ac9413617780a29
Entalizen <danielarya185@gmail.com>
2020-10-02 20:06:21 +0700

    Add implement user model in user domain to register func

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,8 +1,11 @@
 package domain
 
 import (
+	"fmt"
+
 	"github.com/Satssuki/CleanArchSimplified/internal/interfaces"
 	"github.com/Satssuki/CleanArchSimplified/internal/usecase"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // UserLogic wrapper for logic user
@@ -24,5 +27,20 @@ func (l *UserLogic) Compose(s *interfaces.Serializer) error {
 // first return value is response to the user whats happend in the server
 // check cmd/application/handler/helpers.go
 func (l *UserLogic) Register() (string, error) {
-	return "conflict->user already exist", nil
+	exist, err := l.user.IsExist()
+	var userResponse = "internal-error->server does not serve properly"
+
+	if err == nil {
+		if !exist {
+			var oID *primitive.ObjectID
+
+			oID, err = l.user.Insert()
+			if err == nil {
+				userResponse = fmt.Sprintf("created->user has been created with %v", oID)
+			}
+		} else {
+			userResponse = "conflict->user already exist"
+		}
+	}
+	return userResponse, err
 }

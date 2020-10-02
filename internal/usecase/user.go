320dd270e060d13590b2480c2155aca8d7528614
Entalizen <danielarya185@gmail.com>
2020-10-02 20:05:39 +0700

    Add user function isexist and insert

--- internal/usecase/user.go
+++ internal/usecase/user.go
@@ -1,14 +1,45 @@
 package usecase
 
-import "github.com/Satssuki/CleanArchSimplified/internal/interfaces"
+import (
+	"github.com/Kamva/mgm/v3"
+	"github.com/Satssuki/CleanArchSimplified/internal/interfaces"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 // User struct to storing user info
 type User struct {
-	Username  string `json:"username"`
-	LikeCount uint   `json:"likeCount"`
+	mgm.DefaultModel
+	Username  string `json:"username" bson:"username"`
+	LikeCount uint   `json:"likeCount" bson:"likeCount"`
 }
 
 // Serialize as you can see user serialize self by serializer method
 func (u *User) Serialize(s *interfaces.Serializer) error {
 	return s.Transform(u)
+}
+
+// IsExist check username if already exits
+func (u *User) IsExist() (bool, error) {
+	// future safety
+	// i must check the u.Username is nil or not
+	// because it will caused panic
+	count, err := mgm.Coll(u).CountDocuments(mgm.Ctx(), bson.M{
+		"username": u.Username,
+	})
+	return count > 0, err
+}
+
+// Insert user document to the database
+func (u *User) Insert() (*primitive.ObjectID, error) {
+	result, err := mgm.Coll(u).InsertOne(mgm.Ctx(), u)
+	var objectInsertedID *primitive.ObjectID = nil
+
+	if err == nil {
+		v, x := result.InsertedID.(primitive.ObjectID)
+		if x {
+			objectInsertedID = &v
+		}
+	}
+	return objectInsertedID, err
 }

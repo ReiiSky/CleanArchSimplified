d127632d8462408042b1857fd9ebc9aecad6f05f
Entalizen <danielarya185@gmail.com>
2020-10-05 16:18:40 +0700

    Cut logger in RunServer func

--- cmd/application/server.go
+++ cmd/application/server.go
@@ -6,35 +6,23 @@ import (
 	"github.com/Satssuki/CleanArchSimplified/cmd/application/handler"
 	"github.com/Satssuki/CleanArchSimplified/internal/infrastructure"
 	"github.com/labstack/echo/v4"
-	"github.com/rs/zerolog/log"
 )
 
 // RunServer run server with specified port
-func RunServer(port string) {
+func RunServer(port string) error {
+	infrastructure.LoadDevEnvironment()
 	infrastructure.SetupMongoConnection(os.Getenv("DBNAME"), os.Getenv("MONGOURL"))
 	client, err := infrastructure.CreateMongoConnection()
 	err = infrastructure.MongoClientPing(client)
 
-	if err != nil {
-		log.
-			Panic().
-			Msgf("error connect to database with message '%v'", err)
+	if err == nil {
+		infrastructure.SetDefaultClient(client)
+		server := echo.New()
+		registerRoute(server)
 
-		panic(err)
-	} else {
-		log.
-			Info().
-			Msg("mongo successfully connected")
+		server.Start(port)
 	}
-
-	infrastructure.SetDefaultClient(client)
-	server := echo.New()
-	registerRoute(server)
-
-	log.
-		Info().
-		Msgf("server running on port %v", port)
-	server.Start(port)
+	return err
 }
 
 func registerRoute(root *echo.Echo) {

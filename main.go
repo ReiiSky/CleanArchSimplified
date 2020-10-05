f9cc74526c9a34e463c5905bcaf1fbf6eb9033f9
Entalizen <danielarya185@gmail.com>
2020-10-05 16:19:33 +0700

    Refactor main.go to put load env var in cmd/application

--- main.go
+++ main.go
@@ -4,11 +4,8 @@ import (
 	"os"
 
 	"github.com/Satssuki/CleanArchSimplified/cmd/application"
-	config "github.com/Satssuki/CleanArchSimplified/pkg/system"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	godotenv.Load(config.CrossPlatformPath("config/dev.env"))
 	application.RunServer(os.Getenv("PORT"))
 }
